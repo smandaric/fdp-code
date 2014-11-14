c70592486768f0f8c54081e0e41c34e18368c3e8
Stefan <smandaric@users.noreply.github.com>
2014-11-14 20:21:49 +0100

    Add slope
    Added slope method and adjusted for dynamical initial conditions

--- parm-fit/findEdge.m
+++ parm-fit/findEdge.m
@@ -1,12 +1,14 @@
 function [rEdge] = findEdge(uLabSpace,xLabSpace,tLabSpace,method)
 
 frontIndex = zeros(1,length(tLabSpace));
+initialconc = uLabSpace(1,1,2);
 
 switch method
-    case 'tresh'
-
+    case 'tresh'  % finds a value at z=3 above treshhold which is half of initial
+        
+        
         for i = 1:length(tLabSpace)
-            index = find(uLabSpace(i,:,3)>.1/2, 1, 'first'); % for each timepoint, find first index of first concentration above treshhold. Returns empty if no value above treshhold
+            index = find(uLabSpace(i,:,3)>initialconc/2, 1, 'first'); % for each timepoint, find first index of first concentration above treshhold. Returns empty if no value above treshhold
             
             if isempty(index)
                 frontIndex(i) = find(uLabSpace(1,:,2)<.1, 1, 'first') -1 ; % If frontIndex is empty, front is still at initial position, and has moved 0 distance
@@ -14,22 +16,57 @@ switch method
                 frontIndex(i) = index;
             end
         end
-    case 'deplete'
+    case 'deplete' % dip in sum of z2+z3
         
-        gelEdge = find(uLabSpace(1,:,2)<.1, 1, 'first') -1 ;
+        gelEdge = find(uLabSpace(1,:,2)<initialconc, 1, 'first') -1 ;
         
         for i = 1:length(tLabSpace)
-            gelConc = uLabSpace(i,1:gelEdge-5,2)+uLabSpace(i,1:gelEdge-5,3);
+            gelConc = uLabSpace(i,1:gelEdge,2)+uLabSpace(i,1:gelEdge,3);
             
-            index = find(gelConc == min(gelConc), 1, 'first');
+            peakIndex = find(gelConc == max(gelConc), 1, 'first');
+            if isempty(peakIndex) || peakIndex == 1
+                peakIndex = gelEdge;
+            end
+            
+            
+            index = find(gelConc(1:peakIndex) == min(gelConc(1:peakIndex)), 1, 'first');
+            
+            
             
             if isempty(index) || index == 1
                 frontIndex(i) = gelEdge ; % If frontIndex is empty, front is still at initial position, and has moved 0 distance
             else
                 frontIndex(i) = index;
             end
+            
+            frontIndex(diff(frontIndex)>1.5e-4) = gelEdge; % very high change - > initial
+            
         end
-end      
+    
+    case 'slope'
+        gelEdge = find(uLabSpace(1,:,2)<initialconc, 1, 'first') -1 ;
+        uLabSpace = uLabSpace(:,1:gelEdge,3);
+        
+        for i = 1:length(tLabSpace)
+            df = diff(uLabSpace(i,:));
+            df = df(df > 0);
+            
+            index = find(df == max(df),1, 'first');
+            
+            if isempty(index)
+                frontIndex = gelEdge;
+            else
+                frontIndex(i) = index;
+            end
+            
+        end
+end
+
+           
+            
+        
+        
+        
         
 rEdge = - xLabSpace(frontIndex) + xLabSpace(frontIndex(1));
 
